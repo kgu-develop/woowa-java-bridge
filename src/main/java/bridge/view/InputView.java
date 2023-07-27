46f75a44e996c6a87c83c6c7d4fac65f4df8b77f
SangBeom-Hahn <hsb990917@gmail.com>
2023-07-27 14:26:29 +0900

    feat: 사용자 다리 길이 입력 기능 구현
    *InputView: 기능 구현

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,15 +1,41 @@
 package bridge.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 public class InputView {
+    private static void printInputLengthMessage() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
 
     /**
      * 다리의 길이를 입력받는다.
      */
     public int readBridgeSize() {
-        return 0;
+        printInputLengthMessage();
+        
+        String length = Console.readLine();
+    
+        validateHasWhiteSpace(length);
+        validateIsNumeric(length);
+        
+        return Integer.parseInt(length);
+    }
+    
+    private void validateHasWhiteSpace(String input) {
+        if (input.chars().anyMatch(Character::isWhitespace)) {
+            throw new IllegalArgumentException("공백을 입력할 수 없습니다.");
+        }
+    }
+    
+    private void validateIsNumeric(String input) {
+        try {
+            Integer.valueOf(input);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("정수만 입력 가능합니다.");
+        }
     }
 
     /**
