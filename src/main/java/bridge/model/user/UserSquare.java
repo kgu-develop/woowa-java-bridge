2cd6b2266043da759f3b1de18047e009bbdf29e7
SangBeom-Hahn <hsb990917@gmail.com>
2023-07-28 16:04:14 +0900

    refactor: 이동할 칸 입력 검증 메서드 추가

--- src/main/java/bridge/model/user/UserSquare.java
+++ src/main/java/bridge/model/user/UserSquare.java
@@ -6,7 +6,20 @@ public class UserSquare {
     private String square;
     
     public UserSquare() {
-        this.square = InputView.readMoving();
+        String moving = InputView.readMoving();
+        validateCharacter(moving);
+        
+        this.square = moving;
+    }
+    
+    private static void validateCharacter(String moving) {
+        if (isInvalidCharacter(moving)) {
+            throw new IllegalArgumentException("윗 칸은 [U], 아래 칸은 [D]를 눌러주세요");
+        }
+    }
+    
+    private static boolean isInvalidCharacter(String moving) {
+        return !moving.equals("U") && !moving.equals("D");
     }
     
     public String getSquare() {
