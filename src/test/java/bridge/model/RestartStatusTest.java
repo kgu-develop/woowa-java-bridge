41558b5e14241c22d2739f57d16e7c293d385c9e
SangBeom-Hahn <hsb990917@gmail.com>
2023-07-30 13:15:04 +0900

    refactor: 검증 테스트 추가

--- src/test/java/bridge/model/RestartStatusTest.java
+++ src/test/java/bridge/model/RestartStatusTest.java
@@ -1,11 +1,31 @@
 package bridge.model;
 
+import bridge.model.bridge.BridgeMaker;
+import bridge.model.bridge.BridgeNumberGenerator;
+import bridge.model.bridge.BridgeRandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static bridge.utils.ExceptionMessage.Input_Exception.INVALID_BRIDGE_RANGE_EXCEPTION;
+import static bridge.utils.ExceptionMessage.Input_Exception.INVALID_INPUT_RESTART_STATUS_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RestartStatusTest {
+    @Test
+    @DisplayName("올바르지 않은 문자를 입력하면 예외가 발생한다.")
+    void throwException_InvalidCharacter() {
+        // given
+        String signal = "G";
+        
+        // then
+        assertThatThrownBy(() -> RestartStatus.getRestartStatusBy(signal))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_INPUT_RESTART_STATUS_EXCEPTION.getMessage());
+    }
+    
     @Test
     @DisplayName("게임을 재시작한다.")
     void restart() {
--- src/test/java/bridge/model/bridge/BridgeMakerTest.java
+++ src/test/java/bridge/model/bridge/BridgeMakerTest.java
@@ -1,13 +1,33 @@
 package bridge.model.bridge;
 
+import bridge.utils.ExceptionMessage;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
+import static bridge.utils.ExceptionMessage.Input_Exception.INVALID_BRIDGE_RANGE_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BridgeMakerTest {
+    @ParameterizedTest
+    @ValueSource(ints = {2, 21})
+    @DisplayName("범위에 맞지 않은 다리 길이를 입력하지 않으면 예외가 발생한다.")
+    void throwException_NotInRangeBridgeSize(int size) {
+        // given
+        BridgeNumberGenerator bridgeNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(bridgeNumberGenerator);
+        
+        // then
+        assertThatThrownBy(() -> bridgeMaker.makeBridge(size))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_BRIDGE_RANGE_EXCEPTION.getMessage());
+    }
+    
     @Test
     @DisplayName("다리 길이를 입력받아 다리를 생성한다.")
     void makeBridge() {
